33322e739886657ecdc82eee42447d2e00cbd3d3
Sourabh Jain <sourabh.jain@nagarro.com>
2022-07-13 01:28:21 +0530

    integrated MUI form

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,11 +1,102 @@
-import React from 'react';
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Box, Button, Stack, TextField } from "@mui/material";
 
-function Error() {
+const validationSchema = yup.object({
+  customerName: yup.string("Enter your name").required("Name is required"),
+  mobileNumber: yup
+    .string("Enter your number")
+    .min(10, "Mobile Number should be of 10 characters length")
+    .max(10, "Mobile Number should be of 10 characters length")
+    .required("Number is required"),
+  address: yup.string("Enter your address").required("Address is required"),
+  description: yup.string("Enter your description"),
+});
+
+export const Error = () => {
+  const { handleSubmit, handleChange, touched, errors, values } = useFormik({
+    initialValues: {
+      customerName: "",
+      mobileNumber: "",
+      address: "",
+      description: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div>
-      Sorry!!! It seems you are accessing restricted resource
+      <form onSubmit={handleSubmit}>
+        <Box
+          component="div"
+          sx={{ mt: 12 }}
+          style={{ borderRadius: 8, backgroundColor: "whitesmoke" }}
+        >
+          <Stack direction="row" mx={3} pt={3} mb={3}>
+            <TextField
+              required
+              fullWidth
+              id="customerName"
+              name="customerName"
+              label="Customer Name"
+              value={values.customerName}
+              onChange={handleChange}
+              error={touched.customerName && Boolean(errors.customerName)}
+              helperText={touched.customerName && errors.customerName}
+            />
+          </Stack>
+          <Stack direction="row" mx={3} mb={3}>
+            <TextField
+              required
+              fullWidth
+              id="mobileNumber"
+              name="mobileNumber"
+              label="Mobile Number"
+              type="mobileNumber"
+              value={values.mobileNumber}
+              onChange={handleChange}
+              error={touched.mobileNumber && Boolean(errors.mobileNumber)}
+              helperText={touched.mobileNumber && errors.mobileNumber}
+            />
+          </Stack>
+          <Stack direction="row" mx={3} mb={3}>
+            <TextField
+              required
+              fullWidth
+              multiline
+              id="address"
+              name="address"
+              label="Address"
+              type="address"
+              value={values.address}
+              onChange={handleChange}
+              error={touched.address && Boolean(errors.address)}
+              helperText={touched.address && errors.address}
+            />
+          </Stack>
+          <Stack direction="row" mx={3} mb={3}>
+            <TextField
+              fullWidth
+              multiline
+              id="description"
+              name="description"
+              label="Description for chemist"
+              type="description"
+              value={values.description}
+              onChange={handleChange}
+              error={touched.description && Boolean(errors.description)}
+              helperText={touched.description && errors.description}
+            />
+          </Stack>
+          <Button color="primary" variant="contained" type="submit">
+            Submit
+          </Button>
+        </Box>
+      </form>
     </div>
   );
-}
-
-export default Error;
+};
--- src/CustomerDetail/CustomerDetail.js
+++ src/CustomerDetail/CustomerDetail.js
@@ -1,93 +1,37 @@
 import React, { useRef, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import axios from "axios";
 import { useFormik } from "formik";
 import { Card } from "primereact/card";
-import { Button } from "primereact/button";
-import { InputText } from "primereact/inputtext";
-import { InputTextarea } from "primereact/inputtextarea";
-import Tooltip from "@mui/material/Tooltip";
+import TextField from "@mui/material/TextField";
 import { ProgressSpinner } from "primereact/progressspinner";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import "./CustomerDetail.css";
-import { Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { SimpleSnackbar } from "../common/Snackbar/FabIntegrationSnackbar";
+import * as yup from "yup";
+import SaveIcon from "@mui/icons-material/Save";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 
 const CustomerDetail = () => {
-  const [customerName, setCustomerName] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [address, setAddress] = useState("");
-  const [description, setDescription] = useState("");
-  const [uploadedFile, setUploadedFile] = useState(null);
-  const [isDisable, setIsDisable] = useState(false);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const [spinner, setSpinner] = useState(false);
   const uploadedFileRef = useRef();
   const navigate = useNavigate();
+  const name = /^[a-zA-Z ]*$/;
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+  const supportedFormat = [
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "application/pdf",
+  ];
   const subtitle = (
     <em>Please upload prescription along with some basic details of yours</em>
   );
-
-  const sendCustomerDetails = (e) => {
-    if (mobileNumber.startsWith(0)) alert("Mobile number cannot start with 0");
-    else if (!(mobileNumber.length === 10))
-      alert("Please provide 10 digit mobile number");
-    else if (!address) alert("Please provide your address");
-    else if (!(mobileNumber && address && uploadedFile))
-      alert("Please provide value for all the inputs");
-    else submit(e);
-  };
-
-  const submit = (e) => {
-    const customerInput = new FormData();
-    const customerInfo = {
-      customerName,
-      mobileNumber,
-      address,
-      description,
-    };
-    customerInput.append("file", uploadedFile);
-    customerInput.append("customerInfo", JSON.stringify(customerInfo));
-    let url = "https://medspharmabe.herokuapp.com/sendEmail";
-    setSpinner(true);
-    axios
-      .post(url, customerInput, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        setSpinner(false);
-        if (res?.status === 200) setSuccess(true);
-        else setError(true);
-        setIsDisable(true);
-        resetForm();
-      })
-      .catch((err) => {
-        setSpinner(false);
-        setError(true);
-        setIsDisable(true);
-        setSuccess(false);
-        setUploadedFile(null);
-      });
-  };
-
-  const errorSnackbar = () => {
-    return (
-      <SimpleSnackbar
-        snackBarProps={{
-          alert: false,
-          message:
-            "Sorry!!! something wrong happened. Please try again after sometime",
-          severity: "error",
-          duration: "4000",
-        }}
-      />
-    );
-  };
-
   const successSnackbar = () => {
     return (
       <SimpleSnackbar
@@ -102,35 +46,111 @@ const CustomerDetail = () => {
       />
     );
   };
-
-  const resetForm = () => {
-    setCustomerName("");
-    setMobileNumber("");
-    setAddress("");
-    setDescription("");
-    setUploadedFile(null);
-    uploadedFileRef.current !== null && (uploadedFileRef.current.value = null);
-  };
-
   const header = (
     <h2 className="pt-4 mb-n1 headerName">Prescription Details</h2>
   );
-  const footer = (
-    <span>
-      <Button
-        label="Save"
-        icon="pi pi-check"
-        className="me-2"
-        onClick={sendCustomerDetails}
+  const errorSnackbar = () => {
+    return (
+      <SimpleSnackbar
+        snackBarProps={{
+          alert: false,
+          message:
+            "Sorry!!! something wrong happened. Please try again after sometime",
+          severity: "error",
+          duration: "4000",
+        }}
       />
-      <Button
-        label="Reset"
-        icon="pi pi-undo"
-        className="p-button-secondary"
-        onClick={resetForm}
-      />
-    </span>
-  );
+    );
+  };
+
+  const validationSchema = yup.object({
+    customerName: yup
+      .string("Enter your name")
+      .matches(name, "Please enter valid name")
+      .required("Name is required"),
+    mobileNumber: yup
+      .string("Enter your number")
+      .matches(phoneRegExp, "Please enter valid phone number")
+      .min(10, "Mobile number should be of 10 characters length")
+      .max(10, "Mobile number should be of 10 characters length")
+      .required("Number is required"),
+    address: yup.string("Enter your address").required("Address is required"),
+    description: yup.string("Enter your description"),
+    file: yup
+      .mixed()
+      .nullable()
+      .required("Please upload prescription")
+      .test(
+        "FILE_SIZE",
+        "Uploaded file is too big",
+        (value) => !value || (value && value.size <= 1024 * 1024)
+      )
+      .test(
+        "FILE_FORMAT",
+        "Uploaded file is not supported. Please upload pdf or image",
+        (value) => !value || (value && supportedFormat.includes(value?.type))
+      ),
+  });
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    touched,
+    errors,
+    values,
+    isValid,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      customerName: "",
+      mobileNumber: "",
+      address: "",
+      description: "",
+      file: null,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values, { resetForm }, isValid) => {
+      // alert(JSON.stringify(values, null, 2));
+      submit(values);
+      resetForm({ values: "" });
+    },
+  });
+
+  const submit = ({
+    customerName,
+    mobileNumber,
+    address,
+    description,
+    file,
+  }) => {
+    const customerInput = new FormData();
+    const customerInfo = {
+      customerName,
+      mobileNumber,
+      address,
+      description,
+    };
+    customerInput.append("file", file);
+    customerInput.append("customerInfo", JSON.stringify(customerInfo));
+    let url = "https://medspharmabe.herokuapp.com/sendEmail";
+    setSpinner(true);
+    axios
+      .post(url, customerInput, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        setSpinner(false);
+        if (res?.status === 200) setSuccess(true);
+        else setError(true);
+      })
+      .catch((err) => {
+        setSpinner(false);
+        setError(true);
+        setSuccess(false);
+      });
+  };
 
   return (
     <Container className="mt-2 mb-3">
@@ -160,86 +180,123 @@ const CustomerDetail = () => {
           <Card
             subTitle={subtitle}
             className="mb-3 card-container"
-            footer={footer}
             header={header}
           >
             {error && errorSnackbar()}
             {success && successSnackbar()}
-            <Row className="mb-3">
-              <Col md={12} sm={12} className="mb-1">
-                <strong htmlFor="customerName">Customer Name *</strong>
-              </Col>
-              <Col md={12} sm={12}>
-                <InputText
-                  id="customerName"
-                  className="w-100"
-                  value={customerName}
-                  onChange={(e) => setCustomerName(e.target.value)}
-                />
-              </Col>
-            </Row>
-            <Row className="mb-3">
-              <Col md={12} sm={12} className="mb-1">
-                <strong htmlFor="mobileNumber">Mobile No. *</strong>
-              </Col>
-              <Col md={12} sm={12}>
-                <InputText
-                  minLength={10}
-                  maxLength={10}
-                  id="mobileNumber"
-                  className="w-100"
-                  value={mobileNumber}
-                  onChange={(e) => setMobileNumber(e.target.value)}
-                />
-              </Col>
-            </Row>
-            <Row className="mb-3">
-              <Col md={12} sm={12} className="mb-1">
-                <strong htmlFor="address">Address *</strong>
-              </Col>
-              <Col md={12} sm={12}>
-                <InputTextarea
-                  className="w-100"
-                  value={address}
-                  id="address"
-                  onChange={(e) => setAddress(e.target.value)}
-                  autoResize
-                />
-              </Col>
-            </Row>
-            <Row className="mb-3">
-              <Col md={12} sm={12} className="mb-1">
-                <strong htmlFor="description">
-                  Additional information for chemist
-                </strong>
-                <Tooltip title="Please provide additional details for chemist if you want something else from prescription">
-                  <Button className="btn-tooltip">i</Button>
-                </Tooltip>
-              </Col>
-              <Col md={12} sm={12}>
-                <InputTextarea
-                  className="w-100"
-                  value={description}
-                  id="description"
-                  onChange={(e) => setDescription(e.target.value)}
-                  autoResize
-                />
-              </Col>
-            </Row>
-            <Row className="mb-3">
-              <Col md={12} sm={12} className="mb-1">
-                <strong htmlFor="prescription">Upload Prescription *</strong>
-              </Col>
-              <Col md={12} sm={12}>
-                <input
-                  type="file"
-                  className="form-control"
-                  name="upload_file"
-                  ref={uploadedFileRef}
-                  onChange={(e) => setUploadedFile(e.target.files[0])}
-                />
-              </Col>
-            </Row>
+            <form onSubmit={handleSubmit}>
+              <Box
+                component="div"
+                style={{ borderRadius: 8, backgroundColor: "whitesmoke" }}
+              >
+                <Stack direction="row" mx={3} pt={3} mb={3}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="customerName"
+                    name="customerName"
+                    label="Customer Name"
+                    value={values.customerName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.customerName && Boolean(errors.customerName)}
+                    helperText={touched.customerName && errors.customerName}
+                  />
+                </Stack>
+                <Stack direction="row" mx={3} mb={3}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="mobileNumber"
+                    name="mobileNumber"
+                    label="Mobile Number"
+                    type="mobileNumber"
+                    value={values.mobileNumber}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.mobileNumber && Boolean(errors.mobileNumber)}
+                    helperText={touched.mobileNumber && errors.mobileNumber}
+                  />
+                </Stack>
+                <Stack direction="row" mx={3} mb={3}>
+                  <TextField
+                    required
+                    fullWidth
+                    multiline
+                    id="address"
+                    name="address"
+                    label="Address"
+                    type="address"
+                    value={values.address}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={touched.address && Boolean(errors.address)}
+                    helperText={touched.address && errors.address}
+                  />
+                </Stack>
+                <Stack direction="row" mx={3} mb={3}>
+                  <TextField
+                    fullWidth
+                    multiline
+                    id="description"
+                    name="description"
+                    label="Description for chemist"
+                    type="description"
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.description && Boolean(errors.description)}
+                    helperText={touched.description && errors.description}
+                  />
+                </Stack>
+                <Stack direction="row" mx={3} mb={1}>
+                  <strong htmlFor="prescription">Upload Prescription *</strong>
+                </Stack>
+                <Stack direction="row" mx={3} mb={1}>
+                  <input
+                    type="file"
+                    className="form-control"
+                    name="file"
+                    ref={uploadedFileRef}
+                    onChange={(e) => setFieldValue("file", e.target.files[0])}
+                  />
+                </Stack>
+                {touched.file && Boolean(errors.file) && (
+                  <Stack direction="row" mx={3}>
+                    {errors.file}
+                  </Stack>
+                )}
+                <Stack
+                  direction={{ xs: "column", sm: "row" }}
+                  mx={3}
+                  mb={2}
+                  pb={3}
+                  pt={2}
+                >
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    // disabled={!isValid}
+                    startIcon={<SaveIcon />}
+                  >
+                    Submit
+                  </Button>
+                  <Button
+                    color="inherit"
+                    sx={{
+                      marginTop: { xs: 2, sm: 0 },
+                      marginLeft: { sm: 2, xs: 0 },
+                    }}
+                    variant="contained"
+                    startIcon={<RestartAltIcon />}
+                    type="button"
+                  >
+                    Reset
+                  </Button>
+                </Stack>
+              </Box>
+            </form>
           </Card>
         </>
       )}
