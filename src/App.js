e537ffefc746989f42b3cb8537bb452f4fdc1537
Sourabh Jain <sourabh.jain@nagarro.com>
2022-07-24 23:30:42 +0530

    integrated navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Container } from "react-bootstrap";
-import { NavHeader } from "./NavHeader/NavHeader";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import CustomerDetail from "./CustomerDetail/CustomerDetail";
 import { SimpleSnackbar } from "./common/Snackbar/FabIntegrationSnackbar";
-import { Error } from "./Components/Error/Error";
 import BasicExample from "./Components/NavBar/NavBar";
 
 function App() {
